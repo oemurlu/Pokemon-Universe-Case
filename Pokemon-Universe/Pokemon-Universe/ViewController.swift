996341d7a9691eaab2ecbb2a7bd6eee25ed25310
oemurlu <oemurlu@gmail.com>
2024-03-26 08:52:33 +0300

    init project

---
+++ Pokemon-Universe/Pokemon-Universe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokemon-Universe
+//
+//  Created by Osman Emre Ömürlü on 26.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
